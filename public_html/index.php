0645bb3e15e7389ed845414e828604457ab390be
stephen wright <stephenewright@gmail.com>
2016-03-01 10:29:42 -0500

    Modified randomize

--- public_html/index.php
+++ public_html/index.php
@@ -1,21 +1,30 @@
 
 <?php
-include 'db_Conn.php';
-echo "<ul>";
+	include 'db_Conn.php';
+
 $sql = "SELECT * FROM RestList ";
 		foreach($db->query($sql) as $row){
-#var_dump($row);
-$test[] = $row[1];
+
+$test[] = $row;
 
 #echo '<li>'.$test.'</li> ';
 }
-echo"</ul>";
 # var_dump($test);
+#var_dump($row);
+#randomize using index from 0 to values in test double array
 
-#randomize using index from 0 to values in test array
-echo '<h1>'.$test[rand(0, count ($test)-1)].'</h1>';
-#echo rand(0, count ($test)-1);
-
-
+#echo '<h1>'.$test[rand(0, count ($test)-1)][1].'</h1>';
+#echo count ($test[0]);
+$genrand = rand(0, count ($test)-1);
+#var_dump ($genrand);
+################
+##AS list items###
+#echo '<ul>';
+#foreach ($test[$genrand] as $v){
+#	echo  '<li>'.$v.'</li>' ;
+#}
+#echo'</ul>';
+#######################
+echo '<h1>'.$test[$genrand][1].'</h1>'."/n".$test[$genrand][2].$test[$genrand][3].$test[$genrand][4];
 
 ?>
